c8ffa0b4dcc6552cd0474887dd365fc5d71e8583
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2022-07-22 20:05:18 +0300

    19 solution

--- solutions.js
+++ solutions.js
@@ -62,3 +62,71 @@ String.prototype.toJadenCase = function () {
    let mergeString = this.split('').join('');
    return mergeString.split(' ').map(s => s[0].toUpperCase() + s.slice(1 , s.length)).join(' ')
 };
+
+// 12) Shortest Word
+function findShort(s){
+  let lengthOfwords = s.split(' ').map(word => word.length)
+  return Math.min(...lengthOfwords)
+ }
+
+// 13) Complementary DNA
+function DNAStrand(dna){
+  //your code here
+  return dna.split('').map(symbols => symbols == 'A' ? 'T': symbols == 'C' ? 'G': symbols == 'G' ? 'C':'A').join('');
+}
+
+// 14) Credit Card Mask
+// return masked string
+function maskify(cc) {
+  let split = cc.split('');
+  return cc.length > 4 ? split.fill('#' , 0 , split.length - 4).join('') : cc;
+}
+
+// 15) Sum of two lowest positive integers
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+  let sortedarr = numbers.sort((a,b) => a - b);
+  return sortedarr[0] + sortedarr[1];
+}
+
+// 16) Beginner Series #3 Sum of Numbers
+function getSum( a,b )
+{
+   //Good luck!
+  let sum = 0;
+  if(a == b){return a}
+  else if(a > b){
+    for(let i = b; i <= a; i++){
+       sum += i;
+    }
+    return sum
+  }
+    else{
+      for(let i = a; i <= b; i++){
+        sum += i;
+    }
+      return sum
+    } 
+}
+
+// 17) Two to One
+function longest(s1, s2) {
+  // your code
+  let s3 = s1.split('').filter((value,index) => s1.indexOf(value) == index).sort()
+  
+  .concat(s2.split('').filter((value,index) => s2.indexOf(value) == index).sort());
+  
+  return s3.filter((value,index) => s3.indexOf(value) == index).sort().join('');
+}
+
+// 18) Friend or Foe?
+function friend(friends){
+  //your code here
+  return friends.filter(friend => friend.length == 4)
+}
+
+// 19) Find the next perfect square!
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  return Math.sqrt(sq) % 1 == 0 ? Math.pow(Math.sqrt(sq) + 1 , 2) : -1;
+};
